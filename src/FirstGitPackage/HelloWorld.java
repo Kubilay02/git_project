bc2ecf9d85de0ee5f2684450bd32ddaf673d1331
M.Kubilay Karba <mkbly02@gmail.com>
2020-03-14 02:07:42 +0000

    second commit

---
+++ src/FirstGitPackage/HelloWorld.java
@@ -0,0 +1,12 @@
+package FirstGitPackage;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hello World");
+	}
+
+}
